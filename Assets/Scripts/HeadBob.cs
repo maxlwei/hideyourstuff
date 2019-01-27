bb3cfdc6e386e0f8ba881f0f3add89d361015bf7
Max Wei <max.wei@alumni.ubc.ca>
2019-01-27 12:24:12 -0800

    MOM

---
+++ Assets/Scripts/HeadBob.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeadBob : MonoBehaviour
+{
+    float spinTimer = 0.5f;
+    float headSpinUpDownSpeed = 55f;
+    [SerializeField]
+    bool headSpinUp = true;
+    bool headSpinDown = false;
+    bool headPause = false;
+    public float spinSpeed;
+
+    bool headBobUp = true;
+
+    float changeDirectionTimer = 2.5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        spinTimer -= Time.deltaTime ;
+        changeDirectionTimer -= Time.deltaTime;
+
+        HeadBobbing();
+        HeadSpinning();
+        //changeHeadDirection();
+    }
+
+    void HeadBobbing()
+    {
+        if (spinTimer < 0)
+        {
+            headBobUp = !headBobUp;
+        }
+        if (headBobUp)
+        {
+            this.transform.Translate(0, 0.0025f, 0);
+        } else
+        {
+            this.transform.Translate(0, -0.0025f, 0);
+        }
+    }
+    
+    void HeadSpinning()
+    {
+        if (headPause)
+        {
+            if (spinTimer < 0)
+            {
+                headPause = false;
+                spinTimer = 1f;
+                if (headSpinUp)
+                {
+                    headSpinUp = false;
+                    headSpinDown = true;
+                } else
+                {
+                    headSpinUp = true;
+                    headSpinDown = false;
+                }
+            }
+        } else
+        {
+            if (spinTimer < 0)
+            {
+                headPause = true;
+                spinTimer = 1f;
+            } else
+            {
+                if (headSpinUp)
+                {
+                    this.transform.Rotate(Vector3.up * Time.deltaTime * -headSpinUpDownSpeed);
+                }
+
+                if (!headSpinUp)
+                {
+                    this.transform.Rotate(Vector3.up * Time.deltaTime * headSpinUpDownSpeed);
+                }
+            }
+        }
+    }
+
+    void changeHeadDirection()
+    {
+        if (changeDirectionTimer < 0)
+        {
+            changeDirectionTimer = 2.5f;
+
+            float x = Random.Range(-20f, 20f);
+            float y = Random.Range(-20f, 20f);
+            Vector3 targetDir = new Vector3(x, y, 0);
+            Vector3 newDir = Vector3.RotateTowards(transform.forward, targetDir, 1f, 1f);
+
+            this.transform.Rotate(newDir);
+        }
+    }
+}
---
+++ Assets/Scripts/WalkOfShame.cs
@@ -0,0 +1,122 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class WalkOfShame : MonoBehaviour
+{
+    //public GameObject parentWayPoints;
+    //private List<GameObject> wayPointList;
+    public GameObject[] wayPointList;
+    int currentWayPoint = 0;
+    public GameObject entranceWayPoint;
+    public GameObject exitWayPoint;
+    GameObject targetWayPoint;
+
+    public Canvas gameOverCanvas;
+
+    public int numberOfMoves = 7;
+    public float turnSpeed = 4f;
+    public float walkSpeed = 4f;
+
+    public float delayBeforeStart = 2;
+    public float waitDelay = 2;
+    float waitTimer;
+
+    bool walkedBackToEntrance = false;
+    
+    private void Start()
+    {
+        /*
+        foreach (Transform child in parentWayPoints.transform)
+        {
+            wayPointList.Add(child.gameObject);
+        }
+        */
+        targetWayPoint = entranceWayPoint;
+    }
+
+    void Update()
+    {
+        waitTimer -= Time.deltaTime;
+        if (delayBeforeStart > 0)
+        {
+            delayBeforeStart -= Time.deltaTime;
+        } else
+        {
+            if (numberOfMoves > 0)
+            {
+                Walk();
+            } else
+            {
+                FinalWalk();
+            }
+        }
+    }
+
+    void Walk()
+    {
+        if (waitTimer < 0)
+        {
+            transform.forward = Vector3.RotateTowards(transform.forward, targetWayPoint.transform.position - transform.position, turnSpeed * Time.deltaTime, 0.0f);
+            transform.position = Vector3.MoveTowards(transform.position, targetWayPoint.transform.position, walkSpeed * Time.deltaTime);
+        }
+    }
+
+    void FinalWalk()
+    {
+        if (walkedBackToEntrance == false)
+        {
+            targetWayPoint = entranceWayPoint;
+            Walk();
+        } else
+        {
+            targetWayPoint = exitWayPoint;
+            Walk();
+        }
+
+    }
+
+    private void OnTriggerStay(Collider collider)
+    {
+        if (targetWayPoint)
+        {
+            Collider target = targetWayPoint.GetComponent<Collider>();
+            if (collider == target)
+            {
+                if (walkedBackToEntrance)
+                {
+                    TriggerGameOver();
+                }
+                if (numberOfMoves < 1)
+                {
+                    walkedBackToEntrance = true;
+                }
+                ResetTimer();
+                SetNewWaypoint();
+                numberOfMoves--;
+            }
+        }
+    }
+
+    void ResetTimer()
+    {
+        waitTimer = waitDelay;
+    }
+    
+    void SetNewWaypoint()
+    {
+        int tmpWayPoint = currentWayPoint;
+        while (tmpWayPoint == currentWayPoint)
+        {
+            tmpWayPoint = Random.Range(0, wayPointList.Length);
+        }
+        currentWayPoint = tmpWayPoint;
+        targetWayPoint = wayPointList[currentWayPoint];
+    }
+
+    void TriggerGameOver()
+    {
+        gameOverCanvas.gameObject.SetActive(true);
+    }
+}
